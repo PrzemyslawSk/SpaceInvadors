e1b40b5cd0e61d85c7b7ce70e60032822404e883
Przemysław Skoczewski <pskoczew@student.wszib.edu.pl>
2019-03-01 00:27:17 +0100

    initial commit

---
+++ MonoGame-SpaceInvaders/Enemy/Alien.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame_SpaceInvaders.Enemy
+{
+    public class Alien
+    {
+        public Vector2 position;
+        public Texture2D texture;
+        public bool canDraw;
+        int positionUpdateCounter=0;
+
+
+        public Alien()
+        {
+            position = new Vector2(0,0);
+        }
+        public void UpdatePosition(GameTime gameTime)
+        {
+            if(positionUpdateCounter>60)
+            {
+                position.X += 50;
+                positionUpdateCounter = 0;
+            }
+            else
+            {
+                positionUpdateCounter++;
+            }
+        }
+    }
+}
